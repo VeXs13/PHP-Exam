8e652bdb077fe91b2a6c07ee710623f22bf98aa0
VeXs <81629093+VeXs13@users.noreply.github.com>
2022-01-28 23:23:04 +0100

    J'ai rien changé depuis la date limite de rendu !!
    J'ai juste fais un test car j'ai changer de pc et je voulais voir si le code que je clonais etait le bon!!!

--- system/get_all_articles.php
+++ system/get_all_articles.php
@@ -1,7 +1,7 @@
 <?php
 include_once('db\connexionDB.php');
 
-    $allArticles = $BDD->query('SELECT id, id_auteur, titre, description, contenu, pseudo_auteur, date_publication FROM articles ORDER BY id ASC LIMIT 0,5');
+    $allArticles = $BDD->query('SELECT id, id_auteur, titre, description, contenu, pseudo_auteur, date_publication FROM articles ORDER BY id DESC LIMIT 0,5');
 
     if(isset($_GET['search']) AND !empty($_GET['search'])) {
         $searching = $_GET['search'];
